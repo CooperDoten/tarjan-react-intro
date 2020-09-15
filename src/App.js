f37334b82d3dd29a0febebd5d352ee2153fb0148
CooperDoten <cooper.doten@gmail.com>
2020-09-15 13:41:33 -0500

    added state to app.js and picturegallery.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,31 @@ import ThingsToLearn from './ThingsToLearn';
 
 // React Component Incantation 
 class App extends Component {
+
+  //initial state when app loads
+  state = {
+    whatDayIsIt: 'Taco Day',
+    feeling: 'Happy',
+  }
+
+  myAwesomeFunction() {
+    console.log('I have state!', this.state);
+  }
   render() {
+
+    // access state
+    console.log('app state is ', this.state);
+    //what this === ... its app
+    console.log('this is', this);
+
+    //Awesome function
+    this.myAwesomeFunction();
+ 
     //returning some JSX
     // JSX === Javascript eXtended
     return (
      <div>
-       <h1>Happy React Day!</h1>
+       <h1>{this.state.feeling} {this.state.whatDayIsIt}!</h1>
        
        <h3>Things to learn about React:</h3>
        <ThingsToLearn />
--- src/PictureGallery.js
+++ src/PictureGallery.js
@@ -4,10 +4,15 @@ import React, { Component } from 'react';
 
 class PictureGallery extends Component {
 
+    state = {
+        photoWidth: 200,
+        photoHeight: 200
+    }
     render() {
         return (
             <div>
                 <img src="https://picsum.photos/id/293/200/200" />
+                <img src={`https://picsum.photos/id/203/${this.state.photoWidth}/${this.state.photoHeight}`} />
                 <img src="https://picsum.photos/id/122/200/200" />
                 <img src="https://picsum.photos/id/23/200/200" />
                 <img src="https://picsum.photos/id/405/200/200" />
